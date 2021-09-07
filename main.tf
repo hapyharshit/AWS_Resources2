c4603bee88d5959bc36aa7e5fac812874253ebcd
Accurics <admin@accurics.com>
2021-09-07 09:12:17 +0530

    Auto Remediation by Accurics, file: main.tf, resource: aws_s3_bucket.accuricsbucketdemo, violation: [HIGH]: Ensure AWS S3 Buckets are accessible to all authenticated users

--- main.tf
+++ main.tf
@@ -40,3 +40,29 @@ resource "aws_s3_bucket" "accuricsbucketdemo" {
     enabled = true
   }
 }
+
+resource "aws_s3_bucket_policy" "accuricsbucketdemoPolicy" {
+  bucket = "${aws_s3_bucket.accuricsbucketdemo.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "accuricsbucketdemo-restrict-access-to-users-or-roles",
+      "Effect": "Allow",
+      "Principal": [
+        {
+          "AWS": [
+            "arn:aws:iam::##acount_id##:role/##role_name##",
+            "arn:aws:iam::##acount_id##:user/##user_name##"
+          ]
+        }
+      ],
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.accuricsbucketdemo.id}/*"
+    }
+  ]
+}
+POLICY
+}
